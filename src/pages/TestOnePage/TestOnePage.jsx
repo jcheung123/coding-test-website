41fe709b9487f4a70c006502d78717c5af60214b
Johnny C <cheungjc@outlook.com>
2022-10-08 21:58:33 -0400

    adding sample data and quiz functionality

--- src/pages/TestOnePage/TestOnePage.jsx
+++ src/pages/TestOnePage/TestOnePage.jsx
@@ -1,13 +1,98 @@
-import React from 'react';
 
-const TestOnePage = () => {
+import React, { Component } from 'react';
+import './TestOnePage.css';
+import { testOneData } from '../../testOneData';
+import Button from 'react-bootstrap/Button';
 
-    return (
-      <div> 
-          <h1>this is the page for test one - javascript</h1>
-      </div>
-    );
+class TestOnePage extends Component {
+  state = {
+    displayQuestion: 0,
+    userSelection: '',
+    choices: [],
+    disabled: true,
+    submitTest: false
+  }
+
+  componentDidMount() {
+    this.setState(() => {
+      return {
+        questions: testOneData[this.state.displayQuestion].question,
+        choices: testOneData[this.state.displayQuestion].choices
+      };
+    });
+  }
+
+  handleNextBtn = () => {
+    this.setState({displayQuestion: this.state.displayQuestion + 1})
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.displayQuestion !== prevState.displayQuestion) {
+      this.setState(() => {
+        return {
+          questions: testOneData[this.state.displayQuestion].question,
+          choices: testOneData[this.state.displayQuestion].choices,
+          disabled: true
+        }
+      })
+    }
+  }
+
+  checkIfSelected = (hasSelected) => {
+    this.setState({ userSelection: hasSelected, disabled: false });
+  };
+
+
+  submitTestHandler = () => {
+    if (this.state.displayQuestion === testOneData.length - 1) {
+      this.setState({submitTest: true})
+    }
+  }
   
+  render() {
+    const { choices, userSelection, displayQuestion, submitTest } = this.state
+    
+    if (submitTest) {
+      return (
+        <div className="quiz-section">
+          <h3>Your Responses Has Been Submitted Successfully.</h3>
+        </div>
+      )
+    } else {
+      return (
+        <div className="quiz-section"> 
+            <h1>JavaScript Multiple Choice Test</h1>
+            <br /><br />
+            <h2>{this.state.questions}</h2>
+
+            {choices.map((c) => (
+              <h4
+                className={userSelection === c ? "selectedStyling" : "choicesStyling"}
+                onClick={() => this.checkIfSelected(c)}
+              >
+                {c}
+              </h4>
+            ))}
+
+            {displayQuestion < testOneData.length - 1 && (
+              <Button variant="primary" size="lg"
+                disabled={this.state.disabled}
+                onClick={this.handleNextBtn}
+              > Next {'>'}
+              </Button>
+            )}
+
+            {displayQuestion === testOneData.length - 1 && (
+              <Button variant="primary" size="lg"
+                disabled={this.state.disabled}
+                onClick={this.submitTestHandler}
+              > Submit Test {'>'}
+              </Button>
+            )}
+        </div>
+      )
+    } 
+  };
+}
 
 export default TestOnePage;
---
+++ src/testOneData.js
@@ -0,0 +1,45 @@
+const testOneData = [
+    {
+        question: 'Which of the following keywords is used to define a variable in Javascript?',
+        choices: ['for', 'let', 'def', 'None of the above'],
+    },
+
+    {
+        question: 'What keyword is used to check whether a given property is valid or not?',
+        choices: ['in', 'is in', 'exists', 'lies'],
+    },    
+    {
+        question: 'Which is used to serialize an object into a JSON string?',
+        choices: ['stringify()', 'parse()', 'convert()', 'serialize()'],
+    },    
+    {
+        question: 'Which ones are closures in JavaScript?',
+        choices: ['variables', 'functions', 'objects', 'all of the above'],
+    },
+    {
+        question: 'Which one is not a JavaScript Framework?',
+        choices: ['NodeJS', 'Vue', 'React', 'SASS'],
+    },
+    {
+        question: 'Which one is to declare an async function in JavaScript?',
+        choices: ['async', 'await', 'setTimeout', 'all of the above'],
+    },
+    {
+        question: 'Which one is used to access HTML elements?',
+        choices: ['getElementbyId()', 'getElementByClassName()', 'Both A and B', 'None of the above'],
+    },
+    {
+        question: 'What type of language is JavaScript',
+        choices: ['Object-Oriented', 'BackEnd', 'Procedural', 'Object-Based'],
+    },
+    {
+        question: 'How are objects compared when using the strict equality operator?',
+        choices: ['References are compared', 'Contents are compared', 'You cannot use the strict equality operator in JS', 'None of the Above'],
+    },
+    {
+        question: 'What does the ... operator do in JavaScript?',
+        choices: ['Delay a function', 'Define a dataset with too many values', 'Spread iterables to individual elements', 'This operator does not exist'],
+    }
+]
+
+export {testOneData}
